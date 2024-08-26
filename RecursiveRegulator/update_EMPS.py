39233d99bfec4a894de6e9a7fe035acd50dd3027
Jinming3 <93009697+Jinming3@users.noreply.github.com>
2024-08-26 11:33:15 +0200

    Add files via upload

--- RecursiveRegulator/update_EMPS.py
+++ RecursiveRegulator/update_EMPS.py
@@ -1,45 +1,31 @@
+""""
+
+"""
 import matplotlib
 import pandas as pd
 import numpy as np
 import torch
 import matplotlib.pyplot as plt
+
 plt.style.use('F:/Project/head/tight.mplstyle')
 import os
 import sys
 import math
 import time
+
 sys.path.append(os.path.join("F:/Project/head/"))
 import header
-from pem import PEM 
+from pem import PEM, PEM_mimo
 from pem import normalize, R2
-from header import MechanicalSystem, ForwardEulerPEM,ForwardEuler
-
-# import matplotlib.pylab as pylab
-#
-# params = {
-#     # 'figure.figsize': (4.8, 3.7),
-#     'axes.labelsize': 11,
-#     'axes.labelpad': 0.5,
-#     'xtick.labelsize': 11,
-#     'ytick.labelsize': 11,
-#     'legend.fontsize': 11,
-#     'legend.labelspacing': 0.1,
-#     'legend.borderpad': 0.2,
-#     'legend.borderaxespad': 0.25,
-#     'legend.handletextpad': 0.3,
-#     'legend.handlelength': 1,
-#     'legend.loc': 'lower right',
-#
-# }
-# pylab.rcParams.update(params)
+from header import MechanicalSystem_i, ForwardEulerPEM, ForwardEuler_i
 
 
-# -- sin/cos wave ---
 def sinwave(dt, time_all, w=0.5):
     out = []
     A = 1
     for k in range(int(time_all / dt)):
         x = A * np.cos(w * k * math.pi * dt)
+        # x = A * np.sin(w*k * math.pi* dt)
         out.append(x)
     return out
 
@@ -50,6 +36,7 @@ def triangle(dt, time_all):
     p = 2
     for k in range(int(time_all / dt)):
         x = 2 * np.abs(k * dt / p - math.floor(k * dt / p + 0.5))  # 2 * -1
+
         out.append(x)
     return out
 
@@ -87,11 +74,10 @@ M, Fv = 95.1089, 203.5034
 Fc, offset = 20.3935, -3.1648
 # Fc, offset = 0, 0  # remove nonlinear part
 satu = 10  # saturation
-system = 'update'
+system = 'update_i'
 dt = 0.005
 time_all = np.array([70])
 changing = np.array([20, 50]) / dt
-
 
 N = int(time_all[-1] / dt)
 time_exp = np.arange(N) * dt
@@ -108,8 +94,6 @@ p_ref = sinwave(dt, time_all)  # ref signal not change
 sig = 'sinwave'
 p_tri = triangle(dt, time_all)
 
-
-# # # # --------------- sudden change in params and in ref -------------------
 sampling = EMPS(dt, pos=0, vel=0, acc=0, u=0)
 for i in range(changing[0]):
     y = sampling.measure(p_ref[i], noise_process=10 ** -4, noise_measure=10 ** -5)
@@ -143,44 +127,21 @@ for i in range(changing[1], N):
     Fc_all.append(Fc)
     ref_signal.append(p_tri[i])
 
-# # --------------------------------------
-# # # --- continuously degenerating ----
-#
-# sampling = EMPS(dt, pos=0, vel=0, acc=0, u=0)
-# for i in range(changing[0]):
-#     y = sampling.measure(p_ref[i])
-#     Y_sys.append(y)
-#     U.append(sampling.u)
-#
-# offset = offset * 0.97
-# for i in range(changing[0], N):
-#     if i % 50 == 0:
-#         M = M - 0.01 * i * dt
-#         Fv = Fv - 0.0001 * i * dt
-#         Fc = Fc - 0.0001 * i * dt
-#     y = sampling.measure(p_ref[i], noise_process=10 ** -2, noise_measure=10 ** -4)
-#     Y_sys.append(y)
-#     U.append(sampling.u)
-#     M_all.append(M)
-#     Fv_all.append(Fv)
-#     Fc_all.append(Fc)
-
-# ----------------------------
-# ------------------ prepare data type ---------
 Y_sys = normalize(Y_sys, 1)
 U = normalize(U, 1)
 Y_sys = np.asarray(Y_sys, dtype=np.float32)
 U = np.asarray(U, dtype=np.float32)
 U = U[:, np.newaxis]
 dt = torch.tensor(dt, dtype=torch.float32)
-u = torch.tensor(U[:, None, :])  # [:, None, :]
+u = torch.tensor(U[:, None, :])
 y = Y_sys[:, np.newaxis]
 
-lr = 0.0001  # not used in PEM updateing
+lr = 0.0001
 
 model_filename = f"{system}"
 initial_filename = f"{system}_initial"
-model = MechanicalSystem(dt=dt)  #
+model = MechanicalSystem_i(dt=dt)  #
+
 x_fit = torch.load(os.path.join("models", initial_filename))
 checkpoint = torch.load(os.path.join("models", model_filename))
 model.eval()
@@ -194,41 +155,45 @@ optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
 model.load_state_dict(checkpoint['model_state_dict'], strict=False)  # , strict=False
 start_time = time.time()
 
-
-simulator0 = ForwardEuler(model=model, dt=dt)
+simulator0 = ForwardEuler_i(model=model, dt=dt)
 with torch.no_grad():
     xhat0 = simulator0(x0, u)
-    xhat0=xhat0.detach().numpy()
+    xhat0 = xhat0.detach().numpy()
     xhat0 = xhat0.squeeze(1)
     yhat0 = xhat0[:, 0]
 print(f"\n NN Train time: {time.time() - start_time:.2f}")
 
-threshold1 = 1 #0.90  # start retrain, R2
+threshold1 = 1  #0.90  # start retrain, R2
 # threshold2 = 0.98  # stop retrain
 threshold2 = 1  # stop retrain
 # threshold2 = 0.97  # stop retrain
-factor = PEM(2, 6, N)
-factor.P_old2 *= 0.09
+n = 2
+ur = 64
+t = n + n * ur + n
+factor = PEM(n, t, N, ur=ur)
+factor.P_old2 *= 9e-2
+
 factor.Psi_old2 *= 0.9
 np.random.seed(3)
-factor.Thehat_old = np.random.rand(6, 1) * 0.01
-factor.Xhat_old = np.array([[2], [0]])
-# update = 5 # original update
-update = 9 # add pem fix
 
+
+factor.Thehat_old = np.random.rand(t, 1) * 1e-2
+
+
+factor.Xhat_old = np.zeros((n, 1))
+# update = 5 # original update
+# update = 9
+update = 1200  # q size == hidden, inside koopman space
+off = 0  #int(35/dt)
 simulator = ForwardEulerPEM(model=model, factor=factor, dt=dt, N=N, update=update,
-                            threshold1=threshold1, threshold2=threshold2)  
+                            threshold1=threshold1, threshold2=threshold2, train=off)
+
 
 start_time = time.time()
 xhat_data = simulator(x0, u, y)
 print(f"\nTrain time: {time.time() - start_time:.2f}")
-# ----- optimization inside NN loop, stepwise --------
 yhat = xhat_data[:, 0]
-Thehat = simulator.Thehat
-stop = simulator.stop
-correction = simulator.correction
-print(f'update at {correction}')
-print(f'stop at {stop}')
+
 
 # ------ <<<--------------------------------------
 print("inference evolution R^2 = ", R2(Y_sys, yhat))
@@ -242,6 +207,9 @@ ax[0].legend(bbox_to_anchor=(0.9, 0.5))
 ax[1].plot(time_exp, Y_sys, 'g', label='y')
 ax[1].plot(time_exp, yhat, 'r--', label='$\hat{y}$')
 ax[1].plot(time_exp[changing], Y_sys[changing], 'kx')
+if off != 0:
+    ax[1].plot(time_exp[off], Y_sys[off], 'bx')
+
 ax[1].set_ylabel("(b)")
 ax[1].legend(bbox_to_anchor=(0.9, 0.6))
 ax[2].plot(time_exp, U, 'k', label='u')
@@ -251,68 +219,3 @@ ax[3].plot(time_exp, ref_signal, 'k', label='ref')  #
 ax[3].legend()
 ax[3].set_ylabel("(d)")
 ax[3].set_xlabel('time(s)')
-# #
-# fig, ax = plt.subplots(6, 1, sharex=True)
-# ax[0].plot(time_exp, Thehat[:, 0], 'g', label='a0')
-# ax[0].plot(time_exp[changing], Thehat[changing, 0], 'kx')
-# # ax[0].plot(time_exp[correction], Thehat[correction, 0], 'yx')
-# # ax[0].plot(time_exp[stop], Thehat[stop, 0], 'mx')
-# ax[0].legend()
-# ax[1].plot(time_exp, Thehat[:, 1], 'g', label='a1')
-# ax[1].plot(time_exp[changing], Thehat[changing, 1], 'kx')
-# ax[1].legend()
-# ax[2].plot(time_exp, Thehat[:, 2], 'b', label='b0')
-# ax[2].plot(time_exp[changing], Thehat[changing, 2], 'kx')
-# ax[2].legend()
-# ax[3].plot(time_exp, Thehat[:, 3], 'b', label='b1')
-# ax[3].plot(time_exp[changing], Thehat[changing, 3], 'kx')
-# ax[3].legend()
-# ax[4].plot(time_exp, Thehat[:, 4], 'k', label='k0')
-# ax[4].plot(time_exp[changing], Thehat[changing, 4], 'kx')
-# ax[4].legend()
-# ax[5].plot(time_exp, Thehat[:, 5], 'k', label='k1')
-# ax[5].plot(time_exp[changing], Thehat[changing, 5], 'kx')
-# ax[5].legend()
-# ax[5].set_xlabel('time(s)')
-# ----------- degenerating physical parameters --------
-# fig, ax = plt.subplots(3, 1, sharex=True)
-# ax[0].plot(M_all, 'g', label='M')
-# ax[0].legend()
-# ax[1].plot(Fc_all, 'k', label='Fc')
-# ax[1].legend()
-# ax[2].plot(Fv_all, 'k', label='Fv')
-# ax[2].legend()
-
-# simulator.y_pem = np.array(simulator.y_pem)
-# simulator.y_pem0 = np.array(simulator.y_pem0)
-# ts = 0.005
-# plt.figure()
-# # plt.plot(time_exp, simulator.y_pem, 'r', label='$\hat{y}_{pem}$')
-# # plt.plot(time_exp, simulator.y_pem0, 'g', label='$\hat{y}_{pem0}$')
-# plt.plot(simulator.y_pem[:, 1]*ts, simulator.y_pem[:, 0], 'r', label=r'$\bar{y}_{pem}$')
-# plt.plot(simulator.y_pem0[:, 1]*ts, simulator.y_pem0[:, 0], 'g', label='PEM disabled')
-# # plt.plot(simulator.y_pem[:, 0], 'r-', label='$\hat{y}_{pem}$')
-# # plt.plot(simulator.y_pem0[:, 0], 'g-', label='$\hat{y}_{pem}0$')
-# # plt.plot(time_exp[correction], simulator.y_pem[correction], 'yx')
-# # plt.plot(time_exp[stop], simulator.y_pem[stop], 'mx')
-# plt.xlabel('time(s)')
-# plt.legend()
-
-# simulator.correction = np.array(simulator.correction)
-# simulator.stop = np.array(simulator.stop)
-# plt.figure()
-# plt.plot(simulator.correction[:, 1], simulator.correction[:, 0], 'r', label='update')  # time_exp,
-# plt.plot(simulator.stop[:, 1], simulator.stop[:, 0], 'b', label='stop')
-# plt.xlabel('time(s)')
-# plt.legend()
-
-# plt.figure()
-# plt.plot(simulator.r2, 'r', label='$R^2$')  # time_exp,
-# plt.xlabel('time(s)')
-# plt.legend()
-#
-# plt.figure()
-# plt.plot(np.abs(simulator.err), 'r', label='$error$')  # time_exp,
-# plt.xlabel('time(s)')
-# plt.legend()
-# plt.show()
