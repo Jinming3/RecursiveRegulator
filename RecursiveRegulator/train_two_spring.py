72c548286a08ca12a473c8bc1e7ff63ed761b6c1
Jinming3 <93009697+Jinming3@users.noreply.github.com>
2025-08-07 11:35:29 +0200

    Update train_two_spring.py

--- RecursiveRegulator/train_two_spring.py
+++ RecursiveRegulator/train_two_spring.py
@@ -1,22 +1,18 @@
-
 import pandas as pd
 import numpy as np
 import torch
 import time
 import matplotlib.pyplot as plt
 import matplotlib
-
 matplotlib.use("TkAgg")
 import os
 import sys
 import math
 from numpy.linalg import inv
-
-
 from header import R2, normalize
 from header import MechanicalSystem_qu
 from header import ForwardEuler
-# -------------
+
 # define the system parameters
 m1 = 20
 m2 = 20
@@ -24,15 +20,12 @@ k1 = 1000
 k2 = 2000
 d1 = 1
 d2 = 5
-Fc = 0.05 # 20.3935
+Fc = 0.05 
 
-def sinwave(dt, i, w=0.5): # 0.1
-    # A = 5.0
-    A = 4#2.0
+def sinwave(dt, i, w=0.5): 
+    A = 4
     x = A * np.cos(w * i * math.pi * dt)
-    # x = A * np.sin(w*i * math.pi* dt)
     return x
-
 
 
 class Motion:
@@ -47,7 +40,6 @@ class Motion:
 
     def get_y(self,  noise_process=0.0, noise_measure=0.0):
         self.u = sinwave(self.dt, i)
-        # self.u = 5.0
         self.acc1 = -(k1 + k2 / m1) * self.pos1 - (d1 + d2) / m1 * self.vel1 + k2 / m1 * self.pos2 + d2 / m1 * self.vel2 - Fc / m1 * np.sign(
             self.vel1)
         self.acc2 = k2 / m2 * self.pos1 + d2 / m2 * self.vel1 - k2 / m2 * self.pos2 - d2 / m2 * self.vel2 + 1 / m2 * self.u - Fc / m2 * np.sign(
@@ -90,12 +82,12 @@ def vel(pos):
     return v_est
 
 v_est = vel(Y_sys)
-dt = torch.tensor(dt, dtype=torch.float32)  #
+dt = torch.tensor(dt, dtype=torch.float32) 
 # # -----------------------------------------------------------------------
 system = 'two_spring_motion5_8_qb'
 num_epoch = 10000
 batch_num = 64
-batch_length = 64#32
+batch_length = 64
 weight = 1.0  # initial state weight in loss function
 lr = 0.0001
 # state space
@@ -175,7 +167,6 @@ torch.save({'model_state_dict': simulator.model.state_dict(),
            os.path.join("models", model_filename))
 
 torch.save(x_fit, os.path.join("models", initial_filename))
-
 
 
 # initial state estimate
